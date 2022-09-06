e809a603624d9163a8feb95066acde9e20e1d7d2
Adony Montejo <emontejom1@miumg.edu.gt>
2022-09-06 08:58:03 -0600

    tiene errores

--- Forms/FormMedic.php
+++ Forms/FormMedic.php
@@ -11,6 +11,7 @@
 
 
 
+
 <body id="Formresp">
 
     <!-- Interfaz de formulario de Medicos-->
@@ -21,35 +22,74 @@
             <h4>
 
                            <!-- recordar colocar el autocomplete en off -->
+                <table>
+                    <tr>
+                        <td>Nombre</td>
+                        <td><input type="text" name="Nombre_medico" maxlength="50" placeholder="Nombre" style="width: 290%;" required></td>   
+                    </tr>
+
+                    <tr>
+                        <td>Razon Social</td>
+                        <td><input type="text" name="Razon_social_medico" placeholder="Razón Social" required></td>
+                    </tr>
+
+
+
+                        <td>Especialidad</td>
+                        <!-- <td><input type="number" name="Id_especialidad" min="1" max="4"  placeholder="Especialidad"></td>
+                        -->
+
+                        <td><select name="Id_especialidad" >
+                                <option hidden selected value="" style="text-decoration-color: rgb(100, 100, 100, 2);">Especialidad</option>
+                            </select></td>
+                   
+
+                        <td>Email</td>
+                        <td><input type="email" name="Email_medico" placeholder="Email" required></td>
+                    </tr>
+
+                    <tr>
+                        <td>Número de Colegiado</td>
+                        <td><input type="number" name="Colegiado_medico" min="0000" max="9999" placeholder="No. de Colegiado" required></td>
+
+                        <td>Número de Teléfono</td>
+                        <td><input type="number" name="Telefono_medico" max="99999999" min="00000000" placeholder="Teléfono" required></td>
+                    
+                    </tr>
+
+
+                    <tr>
+                        <td>Sexo</td>
+                        <td><select name="Sexo_medico" required>
+                            <option hidden selected value=""></Sexo>
+                            <option value="M">Masculino</option>
+                            <option value="F">Femenino</option>
+                        </select></td>
+
+                        <td>Activo</td>
+                        <td><select name="Activo_medico" required>
+                            <option hidden selected value="">Activo</option>
+                            <option value=1>Si</option>
+                            <option value=0>No</option>
+                        </select></td>
+                    </tr>
+
+
+
+                    <!-- <input type="text" name="Direccion_medico" placeholder="Direccion" style="height: 100px;" required><br><br> -->
+
+                    <tr>
+                        <td>Direccion</td>
+                        <td><textarea name="Direccion_medico" style="width: 150%; height: 40px;"></textarea></td>
+                    </tr>
+
+                    <br>Logo<br>
+                    <input type="file" name="Logo_medico" id="file" accept="image/jpg,image/jpeg,image/png"><br><br>
+
+                    <br>
+                </table>
+                    <button type="submit" id="submit" name="Submit_ins_med">Enviar</button>
                 
-
-                <input type="text" name="Nombre_medico" maxlength="50" placeholder="Nombre" required><br><br>               
-                <input type="number" name="Id_especialidad" min="1" max="4"  placeholder="Especialidad" autocomplete="off"><br><br>
-                <input type="text" name="Razon_social_medico" placeholder="Razón Social" required><br><br>
-
-                <input type="email" name="Email_medico" placeholder="Email" required><br><br>
-                <input type="number" name="Colegiado_medico" min="0000" max="9999" placeholder="No. de Colegiado" required><br><br>
-                <input type="number" name="Telefono_medico" max="99999999" min="00000000" placeholder="Teléfono" required><br><br>
-                
-                <select name="Sexo_medico" required>
-                    <option hidden selected value="">Sexo</Sexo>
-                    <option value="M">Masculino</option>
-                    <option value="F">Femenino</option>
-                </select> <br><br>
-                
-                <select name="Activo_medico" required>
-                    <option hidden selected value="">Activo</option>
-                    <option value=1>Si</option>
-                    <option value=0>No</option>
-                </select><br><br>
-
-                <input type="text" name="Direccion_medico" placeholder="Direccion" style="height: 100px;" required><br><br>
-
-                <br>Logo<br>
-                <input type="file" name="Logo_medico" id="file" accept="image/jpg,image/jpeg,image/png"><br><br>
-
-                <br>
-                <button type="submit" id="submit" name="Submit_ins_med">Enviar</button>
             </h4>
         </form>
 
