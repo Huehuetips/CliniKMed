0191d9f116469db9d7aa95c2663affe37ac0b165
Adony Montejo <emontejom1@miumg.edu.gt>
2022-09-06 09:02:55 -0600

    solucion de errores de commit anterior, regreso al ultimo commit valido

--- JS/evitar_reenvio.js
+++ JS/evitar_reenvio.js
@@ -1,4 +1,5 @@
 if (window.history.replaceState) {
-	console.log("")
+	console.log("probando")
+	parent of c2a6b23 (no se que esta pasando)
 	window.history.replaceState(null,null,window.location.href)
 }
